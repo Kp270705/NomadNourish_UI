71bd3dc2eeae5a6e83e143f288bbdd6a342e5f8f
Kp270705 <kunalpathak4774@gmail.com>
2025-11-11 01:11:47 +0530

    adding backend prod url

--- src/config/backend_routes.js
+++ src/config/backend_routes.js
@@ -1,7 +1,8 @@
 const routesType = {
   // current_route: "http://0.0.0.0:8000",
-  current_route: "http://127.0.0.1:8000",
+  // current_route: "http://127.0.0.1:8000",
   // current_route:"https://nomadnourish.onrender.com",
+  current_route:"https://nomadnourish-tgqx.onrender.com",
 
   // current_route: "https://certgenieserver-production.up.railway.app",
 };
